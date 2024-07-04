37551189e604099912f183b1af682d5c8aff1714
Cody Bontecou <bontecouc@gmail.com>
2024-07-04 21:19:21 +0800

    mocking implementation

--- main.ts
+++ main.ts
@@ -3,37 +3,43 @@ import { extractRealStrings } from './lib/extractRealStrings'
 import { matchWords } from './lib/matchWords'
 import { cutVideoAndAddSubtitles } from './lib/cutVideoAndAddSubtitles'
 import { chatRequest, whisperTranscribe } from './lib/whisper'
+import { convertMp4ToMp3 } from './lib/convertMp4ToMp3'
+import whisperResMock from './output.json'
+import chatContentMock from './mocks/response.json'
 
 async function processAudio(audioData) {
   const totalStartTime = performance.now()
 
   try {
-    const whisperRes = await whisperTranscribe(audioData)
+    // const whisperRes = await whisperTranscribe(audioData)
+    const whisperRes = whisperResMock
     console.log(
       `whisperTranscribe took ${performance.now() - totalStartTime} ms`
     )
 
-    const chatRes = await chatRequest(whisperRes.text)
-    console.log(`chatRequest took ${performance.now() - totalStartTime} ms`)
+    // const chatRes = await chatRequest(whisperRes.text)
 
-    const jsonData = JSON.stringify(chatRes.choices[0])
-    const transcript = JSON.parse(jsonData)
-    const content = JSON.parse(transcript.message.content)
+    // console.log(`chatRequest took ${performance.now() - totalStartTime} ms`)
 
-    const realStrings = extractRealStrings(whisperRes.text, content.sections)
+    // const jsonData = JSON.stringify(chatRes.choices[0])
+    // const transcript = JSON.parse(jsonData)
+    // const content = JSON.parse(transcript.message.content)
+    const content = chatContentMock
+    // console.log('content: ', content)
+
+    const realStrings = extractRealStrings(whisperRes.text, content.segments)
 
     await Promise.all(
       realStrings.map(async (s, index) => {
         const iterationStartTime = performance.now()
 
         const matchedWords = matchWords(s, whisperRes.words)
-        console.log('matchedWords: ', matchedWords)
 
         const startTime = matchedWords[0].start
         const endTime = matchedWords[matchedWords.length - 1].end
 
         await cutVideoAndAddSubtitles(
-          'mocks/10min.mp4',
+          'mocks/yt.mp4',
           `${index}-fullrun.mp4`,
           startTime,
           endTime,
@@ -56,6 +62,8 @@ async function processAudio(audioData) {
   }
 }
 
-const audioFile = 'mocks/output.mp3'
+const audioFile = 'mocks/yt.mp3'
+// convertMp4ToMp3('mocks/yt.mp4', 'mocks/yt.mp3')
+
 const audioData = fs.readFileSync(audioFile)
 processAudio(audioData)
